892ef6778dbb2cd32a522d02a6adc2c024c08a08
bharathtorch <bharath.kumar@torch.io>
2024-07-18 09:14:39 +0530

    Update matchers.rb

--- lib/wisper/rspec/matchers.rb
+++ lib/wisper/rspec/matchers.rb
@@ -33,7 +33,6 @@ module Wisper
         expected_args = args.size > 0 ? args : [any_args]
         expected_kwargs = kwargs.empty? ? {} : kwargs
         @broadcast_events.any? do |event_params|
-          binding.pry
           name, *args, kwargs = event_params
           matcher = ::RSpec::Mocks::ArgumentListMatcher.new(event_name.to_s, *expected_args, **expected_kwargs)
           matcher.args_match?(name, *args, **kwargs)
